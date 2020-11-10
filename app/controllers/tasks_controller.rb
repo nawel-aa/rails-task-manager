31b1a05d77bf17289b3535c51dfc5bf16faf6262
Nawel Aammar <nawel.aa@hotmail.fr>
2020-11-10 13:38:55 -0500

    Destroy route created

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  before_action :find_task, only: [:show, :edit]
+  before_action :find_task, only: %i[show edit update destroy]
 
   def index
     @tasks = Task.all
@@ -20,6 +20,16 @@ class TasksController < ApplicationController
   end
 
   def edit
+  end
+
+  def update
+    @task.update(task_params)
+    redirect_to task_path(@task.id)
+  end
+  
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # CREATE
-  get 'tasks/new', to: 'tasks#new', as: :new_task
-  post 'tasks', to: 'tasks#create'
+  get '/tasks/new', to: 'tasks#new', as: :new_task
+  post '/tasks', to: 'tasks#create'
 
   # READ
-  get 'tasks', to: 'tasks#index'
-  get 'tasks/:id', to: 'tasks#show', as: :task
-
-  # UPDATE
-  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
-  patch 'tasks/:id', to: 'tasks#update'
+  get '/tasks', to: 'tasks#index'
+  get '/tasks/:id', to: 'tasks#show', as: :task
 
   # DELETE
+  delete '/tasks/:id', to: 'tasks#destroy'
+  
+  # UPDATE
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch '/tasks/:id', to: 'tasks#update'
+
 end
