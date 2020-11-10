c2da011dd52cb9aa01bbba5576a7383988ec1e61
Nawel Aammar <nawel.aa@hotmail.fr>
2020-11-10 13:10:13 -0500

    Routes for creating a task created

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   before_action :find_task, only: [:show]
-  
+
   def index
     @tasks = Task.all
   end
@@ -8,9 +8,24 @@ class TasksController < ApplicationController
   def show
   end
 
+  def new
+    @task = Task.new
+  end
+
+  def create
+    task = Task.new(task_params)
+    task.save
+
+    redirect_to tasks_path(task.id)
+  end
+
   private
 
   def find_task
     @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :details)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # CREATE
+  get 'tasks/new', to:'tasks#new'
+  post 'tasks', to: 'tasks#create'
 
   # READ
   get 'tasks', to: 'tasks#index'
